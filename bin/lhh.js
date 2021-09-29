afd2a2d9d501924f352b515fcb51e060eedc2985
李洪辉 <yishui.lhh@raycloud.com>
2021-09-29 13:54:13 +0800

    init

--- bin/lhh.js
+++ bin/lhh.js
@@ -1,1 +1,16 @@
-console.log('hello');
+#!/usr/bin/env node
+const program = require('commander');
+const pkg = require('../package.json');
+console.log('lhh', process.env, pkg)
+
+program
+    .version(pkg.version)
+    .description('启动 kdzs-cli')
+    .option('dev', '测试环境打包')
+    .option('build_p64', 'p64环境打包')
+    .option('build', '正式环境打包')
+    .option('start', '启动开发')
+    .option('start_p64', '启动开发')
+    .option('init', '初始化项目配置文件')
+    .option('create', '创建项目')
+    .parse(process.argv);
